941f504818253af39c5f2aa6eb839ccb36e0b5ef
Joost Papendorp <you@example.com>
2022-08-30 20:16:47 +0200

    Exercise 7.5 and 7.6 Reversing positional list w/o actually using positions. Shuffling indexed and positional lists.

--- src/main/java/nl/papendorp/chapter7/exercises/Exercises_Chapter_7.java
+++ src/main/java/nl/papendorp/chapter7/exercises/Exercises_Chapter_7.java
@@ -4,6 +4,9 @@ package nl.papendorp.chapter7.exercises;
 import net.datastructures.LinkedPositionalList;
 import net.datastructures.PositionalList;
 
+import java.util.Random;
+import java.util.function.Function;
+
 public class Exercises_Chapter_7
 {
 	public static < TYPE > PositionalList< TYPE > reverse_7_5a( final PositionalList< TYPE > list )
@@ -40,6 +43,68 @@ public class Exercises_Chapter_7
 		for( var i = 0; i < size; i++ )
 		{
 			list.addFirst( list3.remove( list3.first() ) );
+		}
+
+		return list;
+	}
+
+	public static < TYPE > java.util.List< TYPE > shuffleIndexed_7_6( final java.util.List< TYPE > list )
+	{
+		// gegeven:  randomInteger( k ) = x, met x <- random( 0..k-1 ), zoals:
+		final Function< Integer, Integer > randomInteger = new Random( System.currentTimeMillis() )::nextInt;
+
+		// gevraagd: shuffle een list met length = size
+		final var size = list.size();
+
+		// i = index of next element to swap with random element from remainder of the list
+		// last element needs no swapping
+		for( var i = 0; i < size - 1; i++ )
+		{
+			// size of list remainder. element can remain constant (i.e. swapped with itself)
+			final var remaining = size - i;
+
+			// choose random index from the remainder: i..length (exclusive)
+			var nextIndex = i + randomInteger.apply( remaining );
+
+			//swap
+			final var temp = list.get( i );
+			list.set( i, list.get( nextIndex ) );
+			list.set( nextIndex, temp );
+		}
+
+		return list;
+	}
+
+	public static < TYPE > PositionalList< TYPE > shufflePositional_7_6( final PositionalList< TYPE > list )
+	{
+		// given:  randomInteger( k ) = x, for x <- random( 0..k-1 ), zoals:
+		final Function< Integer, Integer > randomInteger = new Random( System.currentTimeMillis() )::nextInt;
+
+		// required: shuffle a list with length = size
+		final var size = list.size();
+
+		// elements left to swap
+		// last element needs no swapping
+		var current = list.first();
+		for( var remaining = size; remaining >= 1; remaining -- )
+		{
+			// choose the amount of steps from the current position to swap with: 0..remaining (exclusive
+			var steps = randomInteger.apply( remaining );
+
+			// find the position to swap with, possibly current (steps == 0)
+			var swapPosition = current;
+			for( int j = 0; j< steps; j++)
+			{
+				swapPosition = list.after( swapPosition );
+				if( swapPosition == null )
+					throw new IllegalStateException( remaining + " " + steps );
+			}
+
+			//insert swapped element before current, or skip current
+			if( current != swapPosition )
+				list.addBefore( current, list.remove( swapPosition ) );
+			else
+				current = list.after( current );
 		}
 
 		return list;
--- src/test/java/nl/papendorp/chapter7/exercises/Exercises_Chapter_7_Suite.java
+++ src/test/java/nl/papendorp/chapter7/exercises/Exercises_Chapter_7_Suite.java
@@ -1,10 +1,14 @@
 package nl.papendorp.chapter7.exercises;
 
 import net.datastructures.LinkedPositionalList;
+import net.datastructures.PositionalList;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static nl.papendorp.chapter7.exercises.Exercises_Chapter_7.shuffleIndexed_7_6;
+import static nl.papendorp.chapter7.exercises.Exercises_Chapter_7.shufflePositional_7_6;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -39,6 +43,7 @@ class Exercises_Chapter_7_Suite
 
 		assertFalse( it.hasNext() );
 	}
+
 	@DisplayName( "7.5 b reverse the positional list using size, first, remove and addFirst" )
 	@Test
 	void reverse_7_5b()
@@ -65,5 +70,48 @@ class Exercises_Chapter_7_Suite
 		}
 
 		assertFalse( it.hasNext() );
+	}
+
+	@DisplayName( "7.6 shuffle an indexed list" )
+	@Test
+	void shuffle_indexed_list_7_6()
+	{
+		var given = new java.util.ArrayList< Integer >( 10 );
+
+		// able to reverse non-empty lists
+		for( int i = 0; i < 10; i++ )
+		{
+			given.add( i );
+		}
+
+		final java.util.List< Integer > result = shuffleIndexed_7_6( given );
+
+		for( var next : result )
+			System.out.print( next + " " );
+		System.out.println();
+
+		assertThat( result, containsInAnyOrder( 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ) );
+
+	}
+
+	@DisplayName( "7.6 shuffle a positional list" )
+	@Test
+	void shuffle_positional_list_7_6()
+	{
+		var given = new LinkedPositionalList< Integer >();
+
+		// able to reverse non-empty lists
+		for( int i = 0; i < 10; i++ )
+		{
+			given.addLast( i );
+		}
+
+		final PositionalList< Integer > result = shufflePositional_7_6( given );
+
+		for( var next : result )
+			System.out.print( next + " " );
+		System.out.println();
+
+		assertThat( result, containsInAnyOrder( 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ) );
 	}
 }
